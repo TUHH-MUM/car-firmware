7cd6c8c2e49c375fce01065ab456a3cd38dd3615
Tim <t.hansen@tu-harburg.de>
2018-10-28 05:59:24 +0100

    update cleanup ex0

--- documentation/Exercise 0/track_plot.m
+++ documentation/Exercise 0/track_plot.m
@@ -43,13 +43,13 @@ for i=1:length(median_x)
     y_pos_car_clean(i)=y_pos_car.Data(pos);
 end
 
-%error
+
 x_pos_interpolated=interp1(linspace(1,length(x_pos_car_reduced),length(median_x)),median_x,1:length(x_pos_car_reduced)+1,'spline');
 y_pos_interpolated=interp1(linspace(1,length(y_pos_car_reduced),length(median_x)),median_y,1:length(y_pos_car_reduced)+1,'spline');
-% u_neu=interp1(t_alt,u_alt,t_neu,'spline' )
+
 x_pos_interpolated=x_pos_interpolated(1:end-1);
 y_pos_interpolated=y_pos_interpolated(1:end-1);
-%plot(x_pos_interpolated, y_pos_interpolated,'r--');
+
 plot(-y_pos_car_clean,x_pos_car_clean,'bx');
 
 
@@ -61,38 +61,23 @@ y_car=x_pos_car_reduced;
 y_track=y_pos_interpolated';
 
 %%
-
 for i=1:length(x_track)
-    %x_mean(i)=norm(x_track-x_car);
-    %y_mean(i)=norm(y_track-y_car);
     dist_2norm(i)=norm(sqrt((x_track-x_car).*(x_track-x_car)+(y_track-y_car).*(y_track-y_car)));
     x_track=[x_track(2:end);x_track(1)];
     y_track=[y_track(2:end);y_track(1)];
 end
 [a,b]=min(dist_2norm);
-%[a,b2]=min(y_mean)
-%b=1456
-%for i=1:b
-%    x_car=[x_car(end);x_car(1:end-1)];
-%    y_car=[y_car(end);y_car(1:end-1)];
-%end
+
 for i=1:b
     x_track=[x_track(2:end);x_track(1)];
     y_track=[y_track(2:end);y_track(1);];
 end
 %%
-%hold on
-%plot(x_pos_interpolated,y_pos_interpolated)
-%plot(-y_pos_car.Data,x_pos_car.Data)
-%close
 figure(2)
 hold on
-%plot(x_car,y_car)
-%plot(x_track,y_track)
 dist=sqrt((x_track-x_car).*(x_track-x_car)+(y_track-y_car).*(y_track-y_car));
 plot(x_pos_car.Time(1:place),dist);
 xlabel('time')
 ylabel('distance to track')
-%plot(y_car)
-%plot(y_track)
+
 
